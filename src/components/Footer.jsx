da3c681d946c1f9ae5da9b8a897c7e432a48be6d
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-13 12:06:06 -0500

    copied over the code from the broken app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <span>SeamusQuixote &copy; {new Date().getFullYear()}</span>
+        </div>
+    )
+}
+
+export default Footer
